b7d1cfa78983bc8cba01c92fc0cd07401ab0aa84
shroomcrumpet <kencheng3@gmail.com>
2018-09-18 12:12:03 +0800

    Disabled double-tap zooming (primarily for mobile devices)

--- script.js
+++ script.js
@@ -10,6 +10,23 @@ var dimensions = 4;
 var gameDuration = 15;
 var score = 0;
 
+
+function preventZoom(e) { // prevents double-tap zooming (for mobile devices)
+  var t2 = e.timeStamp;
+  var t1 = e.currentTarget.dataset.lastTouch || t2;
+  var dt = t2 - t1;
+  var fingers = e.touches.length;
+  e.currentTarget.dataset.lastTouch = t2;
+
+  if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+
+  e.preventDefault();
+  e.target.click();
+}
+
+playingField.addEventListener('touchstart', preventZoom);
+
+
 function createBoxes () {
 
     for (i = 0; i < dimensions; i++) {
